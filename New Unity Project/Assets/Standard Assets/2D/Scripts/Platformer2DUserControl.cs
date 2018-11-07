3b34eb52eb729e5e51ec81ae692fdf3b65bf46a1
TaraLemaire <mat0lemaire@gmail.com>
2018-11-07 11:46:54 +0100

    Platform & menu
    plateformes du niveau 1 et menu principal

---
+++ New Unity Project/Assets/Standard Assets/2D/Scripts/Platformer2DUserControl.cs
@@ -0,0 +1,40 @@
+using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace UnityStandardAssets._2D
+{
+    [RequireComponent(typeof (PlatformerCharacter2D))]
+    public class Platformer2DUserControl : MonoBehaviour
+    {
+        private PlatformerCharacter2D m_Character;
+        private bool m_Jump;
+
+
+        private void Awake()
+        {
+            m_Character = GetComponent<PlatformerCharacter2D>();
+        }
+
+
+        private void Update()
+        {
+            if (!m_Jump)
+            {
+                // Read the jump input in Update so button presses aren't missed.
+                //m_Jump = CrossPlatformInputManager.GetButtonDown("Jump"); sauter avec le bouton espace
+                m_Jump = Input.GetKey(KeyCode.UpArrow);
+            }
+        }
+
+
+        private void FixedUpdate()
+        {
+            float h = CrossPlatformInputManager.GetAxis("Horizontal"); // bouger avec les flèches gauche et droite
+            //float h = CrossPlatformInputManager.GetAxis("q d");
+            bool crouch = Input.GetKey(KeyCode.DownArrow);
+            m_Character.Move(h, crouch, m_Jump);
+            m_Jump = false;
+        }
+    }
+}
