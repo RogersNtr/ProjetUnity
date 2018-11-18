5dc4b9c50327430631618b86c79920a8b9a98316
TaraLemaire <mat0lemaire@gmail.com>
2018-11-18 22:13:44 +0100

    controle menu clavier

--- New Unity Project/Assets/MainMenu.cs
+++ New Unity Project/Assets/MainMenu.cs
@@ -4,8 +4,21 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
+    //AudioSource m_MyAudioSource;
+    //AudioSource m_AudioSourceOptions;
+    private void Start()
+    {
+        //m_MyAudioSource = GetComponent<AudioSource>();
+        //m_AudioSourceOptions = GameObject.Find("OptionsMenu").GetComponent<AudioSource>();
+        //m_MyAudioSource.volume = m_AudioSourceOptions.volume;
+    }
 
     public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void Options()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
